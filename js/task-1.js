3a6cbd6c3d2cb9dbe551deb9e952fad4876c9191
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2024-02-23 23:33:26 +0300

    update task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,17 @@
 
-// HTML містить список категорій ul#categories.
-// З використанням властивостей і методів DOM - елементів, напиши скрипт, який:
+const categoriesList = document.querySelectorAll(`.item`);
+const numberOfCategories = categoriesList.length;
 
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
+console.log(`Number of categories: ${numberOfCategories}`);
 
+categoriesList.forEach(item => {
+    const categoriesTitle = item.querySelector(`h2`);
 
-// На що буде звертати увагу ментор при перевірці:
+    console.log(`Category: ${categoriesTitle.textContent}`);
 
-// Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM - елементів
-// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
+    const itemsCategories = item.querySelectorAll(`li`);
+    const numberOfItemsCategories = itemsCategories.length;
+
+    console.log(`Elemens: ${numberOfItemsCategories}`);
+});
+
